7a7ab4f57c75f4db7f1b0a266537f0a4000e6bd7
ohanls24 <sarah.ohanlon24@mail.dcu.ie>
2024-03-13 11:33:19 +0000

    improving magic page

--- simple_test_project01/simple_test_root/simple_test_site/static/script.js
+++ simple_test_project01/simple_test_root/simple_test_site/static/script.js
@@ -3,10 +3,10 @@ document.addEventListener('DOMContentLoaded', function() {
     const toggleNavButton = document.getElementById('toggleNav');
     const navMenu = document.getElementById('nav');
     
-    console.log(toggleNavButton); // Log toggleNavButton to console
+    console.log(toggleNavButton); 
 
     toggleNavButton.addEventListener('click', function() {
-        console.log('Toggle button clicked'); // Log message when button is clicked
+        console.log('Toggle button clicked'); 
         navMenu.classList.toggle('show');
     });
 });
